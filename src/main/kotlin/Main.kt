62751ef6b89091073f865467b37eb46412eb5de4
mbarreirovidal@danielcastelao.org <mbarreirovidal@danielcastelao.org>
2026-09-25 10:32:54 +0200

    Creacion Clase KOTLIN

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+package example.myapp
+
+import example.myapp.example.myapp.Acuario
+
+fun buildAcuario() {
+    val miAcuario = Acuario()
+    miAcuario.printSize()
+    miAcuario.height = 60
+    miAcuario.printSize()
+}
+
+fun main() {
+    buildAcuario()
+}
---
+++ src/main/kotlin/example/myapp/Acuario.kt
@@ -0,0 +1,13 @@
+package example.myapp.example.myapp
+
+class Acuario {
+    var width: Int = 20
+    var height: Int = 40
+    var length: Int = 100
+
+    fun printSize() {
+        println("Ancho: $width cm " +
+                "Largo: $length cm " +
+                "Alto: $height cm ")
+    }
+}

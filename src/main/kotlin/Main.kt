ffe901afdffd9ba652fc9f4cade80ee7e59b3e28
mbarreirovidal@danielcastelao.org <mbarreirovidal@danielcastelao.org>
2026-09-25 11:21:10 +0200

    Agregación Contructores de Clase. KOTLIN

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,27 @@
 package example.myapp
 
-import example.myapp.example.myapp.Acuario
-
 fun buildAcuario() {
-    val miAcuario = Acuario()
-    miAcuario.printSize()
-    miAcuario.height = 60
-    miAcuario.printSize()
+    // 1. Valores por defecto
+    val acuario1 = Acuario()
+    acuario1.printSize()
+
+    // 2. Ancho personalizado
+    val acuario2 = Acuario(width = 25)
+    acuario2.printSize()
+
+    // 3. Alto y largo personalizados
+    val acuario3 = Acuario(height = 35, length = 110)
+    acuario3.printSize()
+
+    val acuario4 = Acuario(width = 25, height = 35, length = 110)
+    acuario4.printSize()
+
+    val acuario6 = Acuario(numeroDePeces = 29)
+    acuario6.printSize()
+    acuario6.volume = 70
+    acuario6.printSize()
 }
 
 fun main() {
     buildAcuario()
-
 }
--- src/main/kotlin/example/myapp/Acuario.kt
+++ src/main/kotlin/example/myapp/Acuario.kt
@@ -1,14 +1,34 @@
-package example.myapp.example.myapp
+package example.myapp
 
-class Acuario {
-    var width: Int = 20
-    var height: Int = 40
-    var length: Int = 100
+class Acuario(
+    var length: Int = 100,
+    var width: Int = 20,
+    var height: Int = 40) {
+    init {
+        println("Iniciando Acuario")
+    }
+
+    init {
+        // 1 Litro = 1000 cm^3
+        println("Volume: ${width * length * height / 1000} l")
+    }
 
     fun printSize() {
-        println("Ancho: $width cm " +
-                "Largo: $length cm " +
-                "Alto: $height cm ")
-
+        println("Ancho: $width cm, Largo: $length cm, Alto: $height cm")
+        //1 l = 1000 cm^3
+        println("Volumen: $volume l")
     }
+
+    constructor (numeroDePeces: Int) : this() {
+        // 2000 cm³ por pez + espacio extra para que no se derrame el agua
+        val tanque = numeroDePeces * 2000 * 1.1
+        // calcular la altura necesaria
+        height = (tanque / (length * width)).toInt()
+    }
+
+    var volume: Int
+        get() = width * height * length / 1000
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
 }
